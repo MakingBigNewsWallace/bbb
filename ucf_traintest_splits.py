96766f03b32b79b717c4122f451ea92698e61b25
ganlipeng <49799922+ganlipeng@users.noreply.github.com>
2020-12-10 20:12:48 +0800

    Add files via upload

--- ucf_traintest_splits.py
+++ ucf_traintest_splits.py
@@ -10,11 +10,12 @@ train_list = [item[0].rstrip('.avi') for item in tmp]
 label = -1;    
 for file_name in sorted(os.listdir(dir_path)):
     label += 1
-    class_path=os.path.join(dir_path,file_name)
+    class_path=os.path.join(dir_path,file_name)+'/i'
     for video_name in sorted(os.listdir(class_path)):
+        #video_path = file_name+'/'+video_name
         video_path = os.path.join(file_name,video_name)
         if video_path in train_list:
             video_path_train = os.path.join(class_path,video_name)
             num = len(os.listdir(video_path_train))
             with open('./ucf101_train_split1.txt','a') as f:
-                f.write(video_path+' '+str(num)+' '+str(label)+'\n');
+                f.write(file_name+'/i/'+video_name+' '+str(num)+' '+str(label)+'\n');
